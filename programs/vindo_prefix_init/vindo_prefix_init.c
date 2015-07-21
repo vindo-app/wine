2f4f29b62b57990a63fa997a0e31c022d06f730d
Theodore Dubois <tblodt@icloud.com>
2015-07-20 17:59:16 -0700

    this should have been in the last commit

---
+++ programs/vindo_prefix_init/vindo_prefix_init.c
@@ -0,0 +1,15 @@
+// Custom program that runs after a new prefix is created. It installs
+// a huge pile of dlls and other stuff, so that everything works.
+//
+// This file is part of Vindo, and is therefore under the same license, which is the GPL.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include <wine/unicode.h>
+#include <wine/debug.h>
+
+int wmain() {
+	return 0;
+}
