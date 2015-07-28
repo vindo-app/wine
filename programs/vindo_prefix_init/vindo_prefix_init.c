8a787d734a427d8a6219a89f9b3f79d0492aff82
Theodore Dubois <tblodt@icloud.com>
2015-07-27 18:11:50 -0700

    more work on v_p_i
    actually, first substantive code

--- programs/vindo_prefix_init/vindo_prefix_init.c
+++ programs/vindo_prefix_init/vindo_prefix_init.c
@@ -9,10 +9,41 @@
 #include <windows.h>
 
 #include "wine/debug.h"
+#include "wine/library.h"
 #include "wine/unicode.h"
 
-WINE_DEFAULT_DEBUG_CHANNEL(vindo_prefix_init);
+const WCHAR *get_dll_dir();
+
+WINE_DEFAULT_DEBUG_CHANNEL(v_p_i);
+
+#define DLL_SRC_TEMPLATE "%s\\dlls\\%s.dll"
+#define DLL_DST_TEMPLATE "C:\\windows\\system32\\%s.dll"
 
 int wmain() {
+    const WCHAR *dll_dir = get_dll_dir();
+
+
+    MESSAGE("dll_dir: %s\n", wine_dbgstr_w(dll_dir));
+
+    HeapFree(GetProcessHeap(), 0, dll_dir);
     return 0;
 }
+
+const WCHAR *get_dll_dir() {
+    const char *data_dir;
+    char *dll_dir;
+
+    if (wine_get_data_dir() != NULL)
+        data_dir = wine_get_data_dir();
+    else {
+        FIXME("wine_get_data_dir returning NULL, using ~/test_data_dir\n");
+        data_dir = "~/test_data_dir";
+    }
+
+    dll_dir = HeapAlloc(GetProcessHeap(), 0, strlen(data_dir) + strlen("/dlls") + 1);
+    strcpy(dll_dir, data_dir);
+    strcat(dll_dir, "/dlls");
+
+    return wine_get_dos_file_name(dll_dir);
+}
+
