c6455d9d08a02f36c53b4a4f973789bf5a5c378f
Theodore Dubois <tblodt@icloud.com>
2015-07-28 18:00:44 -0700

    more work on v_p_i
    it almost does the copy, but some extra backslashes have sneaked into
    the file path, ruining it. I plan to use normal string manipulation
    instead of sprintf.

--- programs/vindo_prefix_init/vindo_prefix_init.c
+++ programs/vindo_prefix_init/vindo_prefix_init.c
@@ -5,27 +5,66 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <windows.h>
+#include <wchar.h>
 
 #include "wine/debug.h"
 #include "wine/library.h"
 #include "wine/unicode.h"
 
 const WCHAR *get_dll_dir();
+void print_error();
 
 WINE_DEFAULT_DEBUG_CHANNEL(v_p_i);
 
-#define DLL_SRC_TEMPLATE "%s\\dlls\\%s.dll"
-#define DLL_DST_TEMPLATE "C:\\windows\\system32\\%s.dll"
 
 int wmain() {
+    static const WCHAR dot_dll[] = {'.','d','l','l',0};
+    static const WCHAR slash_star[] = {'/','*',0};
+    static const WCHAR dest_template[] = {'C','\\','w','i','n','d','o','w','s','\\','s','y','s','t','e','m','3','2','\\','%','s',0};
+    static const WCHAR src_template[] = {'%','s','\\','%','s',0};
+
     const WCHAR *dll_dir = get_dll_dir();
+    WIN32_FIND_DATAW find_data;
+    HANDLE find_handle;
 
+    WCHAR search_pattern[MAX_PATH];
+    strcpyW(search_pattern, dll_dir);
+    strcatW(search_pattern, slash_star);
+    
+    if ((find_handle = FindFirstFileW(search_pattern, &find_data)) == INVALID_HANDLE_VALUE) {
+        print_error();
+        goto stop;
+    }
 
-    MESSAGE("dll_dir: %s\n", wine_dbgstr_w(dll_dir));
+    do {
+        const WCHAR *dll = find_data.cFileName;
+        if (strlenW(dll) > 4 && !strcmpW(dll + strlenW(dll) - 4, dot_dll)) { // extension of .dll
+            WCHAR dest[MAX_PATH];
+            WCHAR src[MAX_PATH];
 
-    HeapFree(GetProcessHeap(), 0, dll_dir);
+            sprintfW(dest, dest_template, dll);
+            sprintfW(src, src_template, dll_dir, dll);
+
+            if (!CopyFileW(src, dest, FALSE)) {
+                print_error();
+                ERR("src: %s", wine_dbgstr_w(src));
+                goto stop;
+            }
+        }
+    } while (FindNextFileW(find_handle, &find_data) != 0);
+
+    if (GetLastError() != ERROR_NO_MORE_FILES) {
+        print_error();
+        goto stop;
+    }
+
+    FindClose(find_handle);
+
+stop:
+    HeapFree(GetProcessHeap(), 0, (LPVOID) dll_dir);
     return 0;
 }
 
@@ -44,6 +83,23 @@ const WCHAR *get_dll_dir() {
     strcpy(dll_dir, data_dir);
     strcat(dll_dir, "/dlls");
 
-    return wine_get_dos_file_name(dll_dir);
+    const WCHAR *dos_dll_dir = wine_get_dos_file_name(dll_dir);
+    HeapFree(GetProcessHeap(), 0, dll_dir);
+    return dos_dll_dir;
 }
 
+void print_error() {
+    char *msg;
+
+    FormatMessageA(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        GetLastError(),
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (char *) &msg,
+        0, NULL );
+
+    ERR("%s", msg);
+}
