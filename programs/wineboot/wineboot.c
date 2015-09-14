469728fb262e45077a464c0fa1074eecb6da51f3
Theodore Dubois <tblodt@icloud.com>
2015-09-14 16:09:30 -0700

    fix some bugs and stuff

--- programs/wineboot/wineboot.c
+++ programs/wineboot/wineboot.c
@@ -1103,7 +1103,6 @@ static HWND show_wait_window(void)
     MultiByteToWideChar( CP_UNIXCP, 0, config_dir, -1, name, len );
     hwnd = CreateDialogParamW( GetModuleHandleW(0), MAKEINTRESOURCEW(IDD_WAITDLG), 0,
                                wait_dlgproc, (LPARAM)name );
-    ShowWindow( hwnd, SW_SHOWNORMAL );
     HeapFree( GetProcessHeap(), 0, name );
     return hwnd;
 }
